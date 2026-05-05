e435e9d74eed8aa77b32d01cffdd14481d237f35
deeelol <tony200404242@gmail.com>
2026-05-06 02:28:35 +0800

    FIre and score.

--- Assets/Scripts/GameCore.cs
+++ Assets/Scripts/GameCore.cs
@@ -263,7 +263,7 @@ public class GameCore
 			bool hasReached1Star = score >= max_score * 0.75;
 			double safeDeltaSeconds = Math.Max(Math.Abs(deltaSeconds), _toleranceSeconds / 4.136);
 			score += (int)(_toleranceSeconds / safeDeltaSeconds * 1000);
-			AnimationController.Instance.UpdateScorebar((double)score / max_score);
+			AnimationController.Instance.UpdateScorebar(Math.Min((double)score / max_score, 1.0));
 			if ((double) score / max_score >= 0.75 && !hasReached1Star) 
 			{
 				AnimationController.Instance.Play1StarEffect();
@@ -272,6 +272,11 @@ public class GameCore
 			Debug.Log($"Beat '{eventType}': {result} ({deltaSeconds:0.000}s)");
 
 			batch.OnCallback(TempoBatchEventType.player_input);
+		}
+		else
+		{
+			score -= 3156;
+			AnimationController.Instance.UpdateScorebar(Math.Max((double)score / max_score, -1.0));
 		}
 		
 		switch (result)
